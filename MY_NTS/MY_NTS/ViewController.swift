8bfb406130d02ab55d011082306b4d697afa138c
Johnson Liu <liu_johnson@yahoo.com>
2021-09-19 10:59:59 -0400

    initial project create.

---
+++ MY_NTS/MY_NTS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MY_NTS
+//
+//  Created by Johnson Liu on 9/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
